bfaa673df86519de12ea040072c8526e87198898
HungTD <danghung.tdh@gmail.com>
2019-09-08 01:27:40 +0700

    commit test UT

--- Test/test-Algorithms.js
+++ Test/test-Algorithms.js
@@ -3,12 +3,16 @@
 const chai = require('chai');
 const expect = chai.expect;
 const twoSum = require('../Algorithms/twoSum');
+const validParentheses = require('../Algorithms/validParentheses');
+const reverse = require('../Algorithms/reverseInteger');
+const intToRoman = require('../Algorithms/integerToRoman');
+
 describe(('Algoritms'), () => {
     beforeEach((done) => {
         done();
     });
     describe(('Two Sum'), () => {
-        let input_Expected = [
+        let input = [
             [
                 [1, 2, 3, 4, 5], 4
             ],
@@ -28,13 +32,68 @@ describe(('Algoritms'), () => {
             [1, 4],
             [0, 2]
         ];
-        for (let index = 0; index < input_Expected.length; index++) {
-            it(`it should return indices ${output_Expected[index]} of the ${input_Expected[index][0]} 
-            to Sum of it is equal ${input_Expected[index][1]}.`, (done) => {
-                let result = twoSum(input_Expected[index][0], input_Expected[index][1]);
+        for (let index = 0; index < input.length; index++) {
+            it(`it should return indices ${output_Expected[index]} of the ${input[index][0]} 
+            to Sum of it is equal ${input[index][1]}.`, (done) => {
+                let result = twoSum(input[index][0], input[index][1]);
                 expect(result).to.eql(output_Expected[index]);
                 expect(result).to.have.lengthOf(2);
                 expect(result).to.be.an('array');
+                done();
+            });
+        }
+    });
+    describe(('Valid Parentheses'), () => {
+        let input = [
+            '()[]{}',
+            '{[]}',
+            '((())){}[',
+            '(({}[]'
+        ];
+        let output_Expected = [
+            true, true, false, false
+        ];
+        for (let index = 0; index < input.length; index++) {
+            it(`it should return ${output_Expected[index]} because ${input[index]} 
+            ${output_Expected[index] == true?'is':'not'} valid Parentheses`, (done) => {
+                let result = validParentheses(input[index]);
+                expect(result).to.eql(output_Expected[index]);
+                done();
+            });
+        }
+    });
+    describe(('Reverse Integer'), () => {
+        let input = [
+            123,
+            4234,
+            12300,
+            -123123
+        ];
+        let output_Expected = [
+            321, 4324, 321, -321321
+        ];
+        for (let index = 0; index < input.length; index++) {
+            it(`it should return ${output_Expected[index]} for reverse ${input[index]}`, (done) => {
+                let result = reverse(input[index]);
+                expect(result).to.eql(output_Expected[index]);
+                done();
+            });
+        }
+    });
+    describe(('Integer to Roman'), () => {
+        let input = [
+            123,
+            4234,
+            12300,
+            1230
+        ];
+        let output_Expected = [
+            "CXXIII", 0, 0, "MCCXXX"
+        ];
+        for (let index = 0; index < input.length; index++) {
+            it(`it should return ${output_Expected[index]} for convert ${input[index]} to Roman`, (done) => {
+                let result = intToRoman(input[index]);
+                expect(result).to.eql(output_Expected[index]);
                 done();
             });
         }
